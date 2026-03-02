38b5faf2beb4e2bb9f88a60d1e1be4bd472bf529
rubenhidalgo-dotcom <ruben.hidalgo@bluewin.ch>
2026-03-02 16:21:06 +0100

    Change response format to include item counts

--- functions/api/status.js
+++ functions/api/status.js
@@ -1,4 +1,11 @@
 export async function onRequestGet({ env }) {
   const list = await env.GIFTS.list();
-  return Response.json(list.keys.map(k => k.name));
+  const result = {};
+
+  for (const k of list.keys) {
+    const val = await env.GIFTS.get(k.name);
+    result[k.name] = JSON.parse(val).count;
+  }
+
+  return Response.json(result);
 }
